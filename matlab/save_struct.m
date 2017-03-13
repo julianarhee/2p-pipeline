09734c24144160e50006cb72f3e7ba930bff5f12
julianarhee <juliana.rhee@gmail.com>
2017-03-13 16:28:49 -0400

    slow, but basic pipeline in place

--- matlab/save_struct.m
+++ matlab/save_struct.m
@@ -1,6 +1,6 @@
 function save_struct(traces_path, traces_fn, T)
 
-save(fullfile(traces_path, traces_fn), 'T', '-struct', '-v7.3');
-fprint('Saved struct %s to %s.\n', traces_fn, traces_path);
+save(fullfile(traces_path, traces_fn), '-struct', 'T', '-v7.3');
+fprintf('Saved struct %s to %s.\n', traces_fn, traces_path);
 
 end
