a656ec578f44824d365c0794044ae1d35f2f72ac
julianarhee <juliana.rhee@gmail.com>
2017-05-24 11:07:49 -0400

    check centroids with masks, incomplete

--- python/plot_roi_centers.py
+++ python/plot_roi_centers.py
@@ -66,14 +66,15 @@ def _tolist(ndarray):
 source = '/nas/volume1/2photon/RESDATA/TEFO'
 session = '20161219_JR030W'
 run = 'retinotopyFinal'
-didx = 5
+didx = 14 #5
 datastruct = 'datastruct_%03d' % didx
 
 
 # Specifiy TIFF paths (if not raw):
 runpath = os.path.join(source, session, run)
-tiffdir = 'DATA/int16'
-tiffpath = os.path.join(runpath, tiffdir)
+#tiffdir = 'DATA/int16'
+#tiffpath = os.path.join(runpath, tiffdir)
+tiffpath = '/nas/volume1/2photon/RESDATA/TEFO/20161219_JR030W/average_volumes/avg_frames_conditions_channel01'
 
 savepath = os.path.join(runpath, 'analysis', datastruct)
 
@@ -85,41 +86,43 @@ if not os.path.exists(savepath):
     os.mkdir(savepath)
 
 
-tiffidx = 3 
-stack = tf.imread(os.path.join(tiffpath, tiffs[tiffidx]))
-
-print "TIFF: %s" % tiffs[tiffidx]
-print "size: ", stack.shape
-print "dtype: ", stack.dtype
-
-# Just get green channel:
-nvolumes = 340
-nslices = 22
-nframes_single_channel = nvolumes * nslices
-channel = 1
-if stack.shape[0] > nframes_single_channel:
-    print "Getting single channel: Channel %02d" % channel
-    volume = stack[::2, :, :]
-else:
-    volume = copy.deepcopy(stack)
-del stack
-
-# Get averaged volume:
-if volume.shape[0] > nslices:
-    print "Averaging across time to produce average volume."
-    avg = np.empty((nslices, volume.shape[1], volume.shape[2]), dtype=volume.dtype)
-    for zslice in range(avg.shape[0]):
-        avg[zslice,:,:] = np.mean(volume[zslice::nslices,:,:], axis=0)
-
-    # Save AVG tif:
-    tf.imsave(os.path.join(savepath, 'avg.tif'), avg)
-
+#tiffidx = 3 
+#stack = tf.imread(os.path.join(tiffpath, tiffs[tiffidx]))
+#
+#print "TIFF: %s" % tiffs[tiffidx]
+#print "size: ", stack.shape
+#print "dtype: ", stack.dtype
+#
+## Just get green channel:
+#nvolumes = 340
+#nslices = 22
+#nframes_single_channel = nvolumes * nslices
+#channel = 1
+#if stack.shape[0] > nframes_single_channel:
+#    print "Getting single channel: Channel %02d" % channel
+#    volume = stack[::2, :, :]
+#else:
+#    volume = copy.deepcopy(stack)
+#del stack
+#
+## Get averaged volume:
+#if volume.shape[0] > nslices:
+#    print "Averaging across time to produce average volume."
+#    avg = np.empty((nslices, volume.shape[1], volume.shape[2]), dtype=volume.dtype)
+#    for zslice in range(avg.shape[0]):
+#        avg[zslice,:,:] = np.mean(volume[zslice::nslices,:,:], axis=0)
+#
+#    # Save AVG tif:
+#    tf.imsave(os.path.join(savepath, 'avg.tif'), avg)
+#
 
 # Load centroids:
 datastruct_fn = 'datastruct_%03d.mat' % didx
 datastruct_path = os.path.join(runpath, 'analysis', datastruct, datastruct_fn)
 
 dstruct = loadmat(datastruct_path)
+
+centers = dstruct['maskInfo']['seeds']
 
 
 
