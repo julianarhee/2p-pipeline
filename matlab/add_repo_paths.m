8e12e4162f855b3d0ea50288c3e5e2c29b52eb2a
Juliana <julianarhee@gmail.com>
2017-06-06 12:07:47 -0400

    check for cvx path in ~/Documents or ~/MATLAB

--- matlab/add_repo_paths.m
+++ matlab/add_repo_paths.m
@@ -3,8 +3,11 @@ addpath(genpath('~/Repositories/2p-tester-scripts'));
 addpath(genpath('~/Repositories/ca_source_extraction'));
 addpath(genpath('~/Repositories/NoRMCorre'));
 addpath(genpath('~/Acquisition2P_class'));
-
-cd ~/Documents/MATLAB/cvx; cvx_setup;
+if ~exist('~/Documents/MATLAB/cvx', 'dir')
+    cd ~/MATLAB/cvx; cvx_setup;
+else
+    cd ~/Documents/MATLAB/cvx; cvx_setup;
+end
 cd ~/Repositories/2p-tester-scripts/matlab
 
 
