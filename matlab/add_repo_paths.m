c06cb2a72117dd7fe685322f9c69ec435c9a5cce
Juliana <julianarhee@gmail.com>
2017-06-30 12:12:08 -0400

    adding missing paths

--- matlab/add_repo_paths.m
+++ matlab/add_repo_paths.m
@@ -2,7 +2,10 @@
 addpath(genpath('~/Repositories/2p-tester-scripts'));
 addpath(genpath('~/Repositories/ca_source_extraction'));
 addpath(genpath('~/Repositories/NoRMCorre'));
-addpath(genpath('~/Acquisition2P_class'));
+addpath(genpath('~/Repositories/Acquisition2P_class'));
+addpath(genpath('~/Repositories/helperFunctions'));
+addpath(genpath('~/Repositories/12k2p-software'));
+
 if ~exist('~/Documents/MATLAB/cvx', 'dir')
     cd ~/MATLAB/cvx; cvx_setup;
 else
