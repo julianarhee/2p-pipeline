ea30c43dfeb116135c927d3b77a36545bfdebc2c
Juliana <julianarhee@gmail.com>
2017-07-22 12:27:56 -0400

    test PSTH on gratings2

--- matlab/create_acquisition_struct.m
+++ matlab/create_acquisition_struct.m
@@ -93,7 +93,7 @@ switch dsoptions.roitype
         roiparams.radius = 1.5;
  
     case '3Dcnmf'
-        roiparams.refidx = 1; % 3;%2;                       % tiff idx to use as reference for spatial components
+        roiparams.refidx = 3; % 3;%2;                       % tiff idx to use as reference for spatial components
         roiparams.tau = [10,18,3] %[2,2,1];                    % std of gaussian kernel (size of neuron) 
         roiparams.p = 0; % 2;                            % order (p = 0 no dynamics, p=1 just decay, p = 2, both rise and decay)
         roiparams.merge_thr = 0.8;                  % merging threshold
