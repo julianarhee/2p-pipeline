58cb94ae600783b881e3fe7624864fb5000407b7
julianarhee <juliana.rhee@gmail.com>
2017-07-27 14:38:57 -0400

    adding additional subset of found/expressing cells, from matlab GUI bootstrap method, creating dict matching neuron IDs to functional IDs

--- python/anatomy2physiology_names.py
+++ python/anatomy2physiology_names.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2
+!/usr/bin/env python2
 
 import os
 import cPickle as pkl
@@ -10,7 +10,7 @@ dictpath = '/nas/volume1/2photon/RESDATA/TEFO/20161219_JR030W/dicts/final'
 target_uuid = 'fcdee2c3-99e9-45dd-8728-fe31730211c5'
 
 
-neuronmap_fn = 'neuronID_map_%s.pkl' % target_uuid 
+neuronmap_fn = 'expressing_neuronID_map_%s.pkl' % target_uuid 
 savepath = os.path.join(dictpath, neuronmap_fn)
 
 
@@ -20,8 +20,15 @@ namedict_path = '/nas/volume1/2photon/RESDATA/TEFO/20161219_JR030W/em7_centroids
 with open(namedict_path) as f:
     names = json.load(f)
 
-matches = [(c['cell_id'], c['neuron_id']) for c in names]
+#matches = [(c['cell_id'], c['neuron_id']) for c in names]
+matches = dict((c['cell_id'], c['neuron_id']) for c in names)
 
+# use bootstrapping to find "true" neurons (matlab, GUI branch):
+path_to_saved_cell_ids = '/nas/volume1/2photon/RESDATA/TEFO/20161219_JR030W/em7_centroids/expressingCells.mat'
+found_cells = scipy.io.loadmat(path_to_saved_cell_ids)
+found_cell_ids = found_cells['cellIDs']
+tmp_cellids = ['cell%04d' % int(i) for i in found_cell_ids]
+cellids = [i for i in tmp_cellids if i in matches.keys()]
 
 # cell_info dict (.pkl) that keys everything with c_uuids, and has 'cell_idx' as a key containing the EM-TEFO alignment name:
 cellinfo_fn = 'cell_info_%s.pkl' % target_uuid
@@ -36,4 +43,10 @@ neuronID_map = dict((cuuid, {'cell_name': cellinfo[cuuid]['cell_idx'], 'neuron_i
 
 with open(savepath, 'wb') as f:
     pkl.dump(neuronID_map, f, protocol=pkl.HIGHEST_PROTOCOL)
+
+
+idkeyfn = '/nas/volume1/2photon/RESDATA/TEFO/20161219_JR030W/em7_centroids/expressing_cell2neuron_key.pkl'
+expressing_cell2neuron_dict = dict((c, matches[c]) for c in cellids)
+with open(idkeyfn, 'wb') as fw:
+    pkl.dump(expressing_cell2neuron_dict, fw, protocol=pkl.HIGHEST_PROTOCOL)
 
