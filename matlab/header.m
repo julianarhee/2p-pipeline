3eccdcd6e0f6b7e1d7f2bee5baca493b7db28cf2
Juliana <julianarhee@gmail.com>
2017-08-28 13:46:35 -0400

    adding header script to set params for motion-correction using Acquisition2P class

---
+++ matlab/header.m
@@ -0,0 +1,26 @@
+% set params for motion-correction, if using Acquisition2P_class
+run_multi_acquisitions = false;
+
+crossref = false;
+processed = true;
+
+source = '/nas/volume1/2photon/RESDATA';
+session = '20170825_CE055';
+run = 'fxnal_data/gratings1';
+
+mc_ref_channel = 1;
+mc_ref_movie = 1;
+
+acquisition_dir = fullfile(source, session, run);
+
+% add repo paths
+add_repo_paths
+fprintf('Added repo paths.\n');
+
+% TODO: set case statements to choose either Acquisition2P or NoRMCorre here.
+motion_correct_acquisitions;
+fprintf('Completed Acqusition2P motion-correction!\n');
+
+
+
+
