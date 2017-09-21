18f1739dbd5e8002ac5e64e9c0d3f4bee5af4dc2
Juliana <julianarhee@gmail.com>
2017-09-21 18:47:59 -0400

    just read each slice's t-series for averaging

--- matlab/preprocessing/create_averaged_slices.m
+++ matlab/preprocessing/create_averaged_slices.m
@@ -1,19 +1,15 @@
 function create_averaged_slices(mcparams)
 
 % Grab (corrected) TIFFs from DATA (or acquisition) dir for correction:
-tiffs = dir(fullfile(mcparams.tiff_dir, '*.tif'));
+data_files = dir(fullfile(mcparams.tiff_dir, '*.tif'));
 fprintf('Creating averaged slice images for TIFFs in:\n%s\n', mcparams.tiff_dir);
-tiff_dir = mcparams.tiff_dir; %D.dataDir;
-tiffs = {tiffs(:).name}';
+data_dir = mcparams.tiff_dir; %D.dataDir;
+data_files = {data_files(:).name}';
 
 path_to_averages = mcparams.averaged_slices_dir;
 
-for tiff_idx=1:length(tiffs)
-    tpath = fullfile(mcparams.tiff_dir, tiffs{tiff_idx});
-    tiffdata = read_file(tpath);
-    d1=size(tiffdata,1); d2=size(tiffdata,2); d3=size(tiffdata,3);
-    fprintf('TIFF %i of %i: size is %s.\n', tiff_idx, length(tiffs), mat2str(size(tiffdata)));
-    
+for tiff_idx=1:length(data_files)
+      
     for ch=1:mcparams.nchannels
         ch_path = fullfile(path_to_averages, sprintf('Channel%02d', ch), sprintf('File%03d', tiff_idx));
         ch_path_vis = fullfile(path_to_averages, sprintf('Channel%02d', ch), sprintf('File%03d_visible', tiff_idx));
@@ -22,17 +18,24 @@ for tiff_idx=1:length(tiffs)
             mkdir(ch_path_vis)
         end 
 	fprintf('Averaging Channel %i, File %i...\n', ch, tiff_idx);
-
+        if mcparams.correct_bidi
+            slice_dir = fullfile(mcparams.tiff_dir, 'Corrected_Bidi', sprintf('Channel%02d', ch), sprintf('File%03d', tiff_idx))
+        else
+            slice_dir = fullfile(mcparams.tiff_dir, 'Corrected', sprintf('Channel%02d', ch), sprintf('File%03d', tiff_idx))
+	end
+        tiffs = dir(fullfile(slice_dir, '*.tif'));
+        tiffs = {tiffs(:).name}'
+        d3 = length(tiffs);
+        sample = read_file(fullfile(slice_dir, tiffs{1}));
+	d1=size(sample,1); d2=size(sample,2); clear sample
+	
 	avgs = zeros([d1,d2,d3]);
 	for sl=1:d3
-	    if strfind(tiffs{tiff_idx}, sprintf('_Channel%02d', ch))    % channels are split
-		avgs(:,:,sl) = mean(tiffdata(:,:,sl,:), 4);
-	    else
-		avgs(:,:,sl) = mean(tiffdata(:,:,sl,ch:mcparams.nchannels:end), 4);
-	    end
+	    tiffdata = read_file(fullfile(slice_dir, tiffs{sl}));
+	    fprintf('TIFF %i of %i: size is %s.\n', tiff_idx, length(tiffs), mat2str(size(tiffdata)));
+	    avgs(:,:,sl) = mean(tiffdata, 3);
 	    slicename = sprintf('average_Slice%02d_Channel%02d_File%03d.tif', sl, ch, tiff_idx);
-	    tiffWrite(avgs(:,:,sl), slicename, ch_path);
-		
+	    tiffWrite(avgs(:,:,sl), slicename, ch_path);	
 	end
 	% make visible
 	tmp = (avgs-min(avgs(:)))./(max(avgs(:))-min(avgs(:)));
