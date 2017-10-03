90798e55bca63661a9e4def414dd90c4f459d511
Juliana <julianarhee@gmail.com>
2017-10-03 19:51:32 -0400

    updated repo name

--- matlab/add_repo_paths.m
+++ matlab/add_repo_paths.m
@@ -1,5 +1,5 @@
 %% Set paths
-addpath(genpath('~/Repositories/2p-tester-scripts'));
+addpath(genpath('~/Repositories/2p-pipeline'));
 addpath(genpath('~/Repositories/ca_source_extraction'));
 addpath(genpath('~/Repositories/NoRMCorre'));
 addpath(genpath('~/Repositories/Acquisition2P_class'));
