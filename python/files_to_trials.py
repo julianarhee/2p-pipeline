fd2ee7552716ae388ab331e79f5e5f39437c4d8f
julianarhee <juliana.rhee@gmail.com>
2017-10-10 15:19:50 -0400

    plot every other ROI, plus draw masks with labelrgb

--- python/files_to_trials.py
+++ python/files_to_trials.py
@@ -11,7 +11,7 @@ import seaborn as sns
 from matplotlib import gridspec
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 import matplotlib.pyplot as plt
-
+import skimage.color
 
 def loadmat(filename):
     '''
@@ -96,7 +96,7 @@ functional_dir = 'functional'
 
 curr_file_idx = 2
 curr_slice_idx = 20
-curr_roi_name = 'blobs_DoG'
+curr_roi_method = 'blobs_DoG'
 
 acquisition_dir = os.path.join(source, experiment, session, acquisition)
 figdir = os.path.join(acquisition_dir, 'example_figures')
@@ -135,21 +135,22 @@ trace_methods_dir = os.path.join(acquisition_dir, 'Traces')
 trace_methods = os.listdir(trace_methods_dir)
 trace_methods = [str(r) for r in trace_methods]
 
-print "Trace methods:"
-trace_methods
+print "Trace methods:", trace_methods
 
 # Get SLICE list:
 if isinstance(ref['slices'], int):
     slice_names = ['Slice01']
 else:
     slice_names = ["Slice{:02d}".format(i+1) for i in range(len(ref['slices']))]
+slice_names = sorted(slice_names, key=natural_keys)
+curr_slice_name = slice_names[curr_slice_idx]
 
 # Get FILE list:
 average_source = 'Averaged_Slices_Corrected'
 signal_channel = 1
 average_slice_dir = os.path.join(acquisition_dir, functional_dir, 'DATA', average_source, "Channel{:02d}".format(signal_channel))
 file_names = [f for f in os.listdir(average_slice_dir) if '_vis' not in f]
-print file_names
+print "File names:", file_names
 nfiles = len(file_names)
 
 # Get AVERAGE slices for current file:
@@ -172,8 +173,8 @@ print "N frames:", nframes
 print "N rois:", nrois
 
 # Get average slice image for current-file, current-slice:
-avg_img_fn = slice_fns[curr_slice_idx]
-avg_tiff_path = os.path.join(curr_slice_dir, avg_img_fn)
+curr_slice_fn = slice_fns[curr_slice_idx]
+avg_tiff_path = os.path.join(curr_slice_dir, curr_slice_fn)
 with tf.TiffFile(avg_tiff_path) as tif:
     avgimg = tif.asarray()
 
@@ -254,6 +255,7 @@ for fi in range(nfiles):
 
 
 # Split all traces by stimulus-ID:
+# ----------------------------------------------------------------------------
 stim_ntrials = dict()
 for stim in stimdict.keys():
     stim_ntrials[stim] = 0
@@ -285,6 +287,7 @@ for stim in stimdict.keys():
     frames_stim_on[stim] = stim_on_frames
 
 
+
 # ---------------------------------------------------------------------------
 # PLOTTING:
 # ----------------------------------------------------------------------------
@@ -292,7 +295,7 @@ for stim in stimdict.keys():
 def get_spaced_colors(n):
     max_value = 16581375 #255**3
     interval = int(max_value / n)
-    colors = [hex(I)[2:].zfill(6) for I in range(0, max_value, interval)]
+    colors = [hex(I)[2:].zfill(6) for I in range(1, max_value, interval)]
 
     return [(int(i[:2], 16), int(i[2:4], 16), int(i[4:], 16)) for i in colors]
 
@@ -302,18 +305,21 @@ spacing = 25
 nstimuli = len(stimdict.keys())
 
 if color_by_roi:
-    colorvals = get_spaced_colors(nstimuli)
+    colorvals255 = get_spaced_colors(nrois)
 else:
-    coorvals = get_spaced_colors(nrois)
-colorvals = np.true_divide(colorvals, 255.)
+    coorvals255 = get_spaced_colors(nstimuli)
+#colorvals255 = colorvals255[1:] 
+colorvals = np.true_divide(colorvals255, 255)
+print len(colorvals255)
 
-
-fig = plt.figure(figsize=(60,5))
-gs = gridspec.GridSpec(plot_rois, 1) #, height_ratios=[1,1,1,1]) 
+plot_rois = np.arange(0, nrois, 2) #int(nrois/2)
+fig = plt.figure(figsize=(nstimuli,int(len(plot_rois)/2)))
+gs = gridspec.GridSpec(len(plot_rois), 1) #, height_ratios=[1,1,1,1]) 
 gs.update(wspace=0.01, hspace=0.01)
-for roi in range(plot_rois): # nrois
+for ridx,roi in enumerate(plot_rois): #np.arange(0, nrois, 2): # range(plot_rois): # nrois
     #rowindex = roi + roi*nstimuli
-    plt.subplot(gs[roi])
+    print "ROI:", roi
+    plt.subplot(gs[ridx])
     plt.axis('off')
     #ax = plt.gca()
         
@@ -321,14 +327,15 @@ for roi in range(plot_rois): # nrois
         #plt.subplot(gs[roi, stimnum])
         #print stim
         raw = traces_by_stim[stim][:, roi, :]
-        avg = np.mean(raw, axis=0)
+        #avg = np.mean(raw, axis=0)
         xvals = np.arange(0, raw.shape[1]) + stimnum*spacing
         #xvals = np.tile(np.arange(0, raw.shape[1]), (raw.shape[0], 1))
-        
+        curr_dfs = np.empty((raw.shape[0], raw.shape[1])) 
         for trial in range(raw.shape[0]):
             frame_on = frames_stim_on[stim][trial][0]
             baseline = np.mean(raw[trial,0:frame_on])
             df = (raw[trial,:] - baseline / baseline)
+            curr_dfs[trial,:] = df
             if color_by_roi:
                 plt.plot(xvals, df, color=colorvals[roi], alpha=1, linewidth=0.2)
             else:
@@ -337,13 +344,15 @@ for roi in range(plot_rois): # nrois
             stim_frames = xvals[0]+frames_stim_on[stim][trial]
             
             plt.plot(stim_frames, np.ones((2,))*-20, color='k')
-            
+        
+        # Plot average:
+        avg = np.mean(curr_dfs, axis=0) 
         if color_by_roi:
             plt.plot(xvals, avg, color=colorvals[roi], alpha=1, linewidth=2)
         else:
             plt.plot(xvals, avg, color=colorvals[stimnum], alpha=1, linewidth=2)
     
-    if roi<plot_rois-1:
+    if roi<len(plot_rois)-1:
         #sns.despine(bottom=True)
         plt.axis('off')
         plt.ylabel(str(roi))
@@ -357,20 +366,28 @@ sns.despine(offset=1, trim=True)
 
 figname = 'all_rois_traces_by_stim.png'
 plt.savefig(os.path.join(figdir, figname), bbox_inches='tight')
-plt.show()
+#plt.show()
 
 # PLOT ROIs:
 img = np.copy(avgimg)
+
+curr_masks = roi_methods_dict[curr_roi_method][curr_slice_name]['masks']
+label_masks = np.zeros((curr_masks.shape[0], curr_masks.shape[1]))
+print label_masks.shape
+roi_idx = 1
+for roi in plot_rois:
+    label_masks[curr_masks[:,:,roi]==1] = int(roi_idx)
+    roi_idx += 1
 
 plt.figure()
 # plt.imshow(img)
 imgnorm = np.true_divide((img - img.min()), (img.max()-img.min()))
 #plt.imshow(imgnorm, cmap='gray'); plt.colorbar()
-plt.imshow(skimage.color.label2rgb(label_masks, image=imgnorm, alpha=0.1, colors=colorvals[1:]*255., bg_label=0))
+plt.imshow(skimage.color.label2rgb(label_masks, image=imgnorm, alpha=0.1, colors=colorvals255, bg_label=0))
 plt.axis('off')
 
 figname = 'all_rois_average_slice.png'
 plt.savefig(os.path.join(figdir, figname), bbox_inches='tight')
-plt.show()
+#plt.show()
 
 
