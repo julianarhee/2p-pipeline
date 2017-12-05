1df3da8037b7ebed6a9f27bd2d2927364ab85fa9
Juliana Rhee <juliana.rhee@gmail.com>
2017-12-04 20:16:18 -0500

    cd to cvx dir inside 2p-pipeline. run cvx_setup.

--- pipeline/matlab/add_repo_paths.m
+++ pipeline/matlab/add_repo_paths.m
@@ -1,20 +1,25 @@
-function add_repo_paths(vargin)
+function add_repo_paths(varargin)
 %% Set paths
-if length(vargin) == 0
+if length(varargin) == 0
     repo_prefix = '~/Repositories';
 else
-    repo_prefix = varargin{1};
+    repo_prefix = varargin{1}
 end
 
 addpath(genpath(fullfile(repo_prefix, '2p-pipeline')));
-addpath(genpath(fullfile(repo_prefix, 'ca_source_extraction')));
-addpath(genpath(fullfile(repo_prefix, 'NoRMCorre')));
+%addpath(genpath(fullfile(repo_prefix, 'ca_source_extraction')));
+%addpath(genpath(fullfile(repo_prefix, 'NoRMCorre')));
 addpath(genpath(fullfile(repo_prefix, 'Acquisition2P_class')));
 addpath(genpath(fullfile(repo_prefix, 'helperFunctions')));
-addpath(genpath(fullfile(repo_prefix, '12k2p-software')));
-if exist(fullfile(repo_prefix, '2p-pipeline', 'pipeline', 'matlab', 'helperfuncs', 'cvx'), 'dir')
-    cd fullfile(repo_prefix, '2p-pipeline', 'pipeline', 'matlab', 'helperfuncs', 'cvs'); cvx_setup;
+%addpath(genpath(fullfile(repo_prefix, '12k2p-software')));
+
+cvx_dir = fullfile(repo_prefix, '2p-pipeline', 'pipeline', 'matlab', 'helperfuncs', 'cvx')
+if exist(cvx_dir, 'dir')
+    cd(cvx_dir);
+    cvx_setup;
 end
-cd fullfile(repo_prefix, '2p-pipeline', 'pipeline')
+pipe_dir = fullfile(repo_prefix, '2p-pipeline', 'pipeline');
 
-
+cd(pipe_dir)
+ 
+end
