b9562636a1a252dec5c88ebe97c899afc6d14640
julianarhee <juliana.rhee@gmail.com>
2017-12-19 12:59:17 -0500

    use updated ROI ID params to run manual2D creation, providing roi_id as manual input

--- pipeline/matlab/rois/get_rois_manual2D.m
+++ pipeline/matlab/rois/get_rois_manual2D.m
@@ -1,29 +1,23 @@
-get_rois_manual2D
 
 clc; clear all;
 % 
 %% Set info manually:
 rootdir = '/nas/volume1/2photon/data';
-animalid = 'JR063';
-session = '20171202_JR063';
-
-roi_slices = '';
-
-rid_hash = 'e4893c';
-zproj_type = 'mean';
+animalid = 'CE059'; %'JR063';
+session = '20171009_CE059'; %'20171202_JR063';
+roi_id = 'rois001'; %'e4893c';
 
 %% Load RID parameter set:
 roi_dir = fullfile(rootdir, animalid, session, 'ROIs');
 roidict = loadjson(fullfile(roi_dir, sprintf('rids_%s.json', session)));
 roi_ids = fieldnames(roidict);
-for rkey = 1:length(fieldnames(roidict))
-    if strcmp(roidict.(roi_ids{rkey}).rid_hash, rid_hash)
-        roi_id = roi_ids{rkey};
-        RID = roidict.(roi_id);
-    end   
-end
-roi_type = RID.roi_type;
+assert(ismember(roi_id, roi_ids), 'ROI ID %s not found in roidict entries at\n%s', roi_id, roi_dir);
+RID = roidict.(roi_id)
 
+roi_type = RID.roi_type;
+zproj_type = RID.PARAMS.options.zproj_type
+rid_hash = RID.rid_hash;
+roi_slices = RID.PARAMS.options.slices
 
 %% Get paths:
 animalroot = strsplit(RID.SRC, session);
@@ -32,18 +26,18 @@ acquisition = sessionparts{2};
 run = sessionparts{3};
 if length(sessionparts(4:end)) > 2
     is_processed = true;
-    tiffsource_name = sessionparts{5};
-    tiffsource_pts = strsplit(tiffsource_name, '_');
-    tiffsource = tiffsource_pts{1};
-    sourcetype_name = sessionparts{6};
-    post_rundir_path = fullfile('processed', tiffsource_name, sourcetype_name);
+    tiffsource = sessionparts{5};
+    tiffsource_pts = strsplit(tiffsource, '_');
+    tiffsource_id = tiffsource_pts{1};
+    source_type = sessionparts{6};
+    post_rundir_path = fullfile('processed', tiffsource, source_type);
 else
     is_processed = false;
-    tiffsource_name = sessionparts{4};
-    tiffsource_pts = strsplit(tiffsource_name, '_');
-    tiffsource = tiffsource_pts{1};
-    sourcetype_name = '';
-    post_rundir_path = tiffsource_name;
+    tiffsource = sessionparts{4};
+    tiffsource_pts = strsplit(tiffsource, '_');
+    tiffsource_id = tiffsource_pts{1};
+    source_type = '';
+    post_rundir_path = tiffsource;
 end
 
 %% Get reference file/channel for manual ROI selection:
@@ -51,20 +45,37 @@ acquisition_dir = fullfile(rootdir, animalid, session, acquisition);
 runinfo = loadjson(fullfile(acquisition_dir, run, sprintf('%s.json', run)));
 if is_processed
     pidinfo = loadjson(fullfile(acquisition_dir, run, 'processed', sprintf('pids_%s.json', run)));
-    if pidinfo.(tiffsource).PARAMS.motion.correct_motion
-        ref_file = pidinfo.(tiffsource).PARAMS.motion.ref_file;
-        ref_channel = pidinfo.(tiffsource).PARAMS.motion.ref_channel;
+    if pidinfo.(tiffsource_id).PARAMS.motion.correct_motion
+        mc_ref_file = pidinfo.(tiffsource_id).PARAMS.motion.ref_file;
+        mc_ref_channel = pidinfo.(tiffsource_id).PARAMS.motion.ref_channel;
     end
 else
-    ref_file = 1;
-    ref_channel = 1;
+    mc_ref_file = 1;
+    mc_ref_channel = 1;
 end
+rid_ref_file = RID.PARAMS.options.ref_file;
+rid_ref_channel = RID.PARAMS.options.ref_channel;
+
+if rid_ref_file~=mc_ref_file
+    fprintf('*****WARNING*****')
+    fprintf('Specified RID reference file does not match MC reference.\n')
+    fprintf('Using ref file %i for ROI extraction (MC: ref file is %i)\n', rid_ref_file, mc_ref_file)
+end
+if rid_ref_channel~=mc_ref_channel
+    fprintf('*****WARNING*****')
+    fprintf('Specified RID reference channel does not match MC reference.\n')
+    fprintf('Using ref channel %i for ROI extraction (MC: ref channel is %i)\n', rid_ref_channel, mc_ref_channel)
+end
+
+ref_file = rid_ref_file;
+ref_channel = rid_ref_channel;
+
 ref_filename = sprintf('File%03d', ref_file);
 ref_channelname = sprintf('Channel%02d', ref_channel);
 fprintf('RID %s -- Using %s, %s as reference for manual ROI creation.\n', rid_hash, ref_filename, ref_channelname);
 
 
-if length(roi_slices) == 0 || ~ismember('slices', fieldnames(RID))
+if length(roi_slices)==0 || ~ismember('slices', fieldnames(RID.PARAMS.options))
     roi_slices = runinfo.slices;
     fprintf('RID %s -- Slices not specified for manual ROI creation. Using all %i slices.\n', rid_hash, length(runinfo.slices));
 else
@@ -82,12 +93,7 @@ else
 end
 slice_tiffs = dir(fullfile(slice_sourcedir, '*.tif'));
 slice_tiffs = {slice_tiffs(:).name}';
-if length(slice_tiffs) == length(roi_slices)
-    fprintf('RID %s -- Found expected number of slice images in:\n%s\n', rid_hash, slice_sourcedir);
-else
-    fprintf('RID %s -- WARNING:  Incorrect number of slices found in specified dir:\n%s\n', rid_hash, slice_sourcedir);
-end
-
+assert(length(slice_tiffs)==length(roi_slices), 'RID %s -- WARNING:  Incorrect number of slices found in specified dir:\n%s\n', rid_hash, slice_sourcedir);
 
 
 %% create empty structure and cells to save roi info
@@ -186,13 +192,13 @@ end
 
 
 %% Save ROI masks by slice:
-mask_fn = fullfile(mask_dir, 'masks.h5');
-
+maskname = sprintf('masks_%s', rid_hash)
+mask_fn = fullfile(mask_dir, sprintf('%s.h5', maskname));
 for slidx = 1:length(sourcepaths)
     masks = allmasks{slidx};
     curr_slice = roi_slices(slidx);
     for curr_roi = 1:size(masks,3)
-        %h5create(mask_fn, sprintf('/masks/slice%i/roi%i', curr_slice, curr_roi), size(masks(:,:,curr_roi)))
+        h5create(mask_fn, sprintf('/masks/slice%i/roi%i', curr_slice, curr_roi), size(masks(:,:,curr_roi)))
         h5write(mask_fn, sprintf('/masks/slice%i/roi%i', curr_slice, curr_roi), masks(:,:,curr_roi))
     end
     h5writeatt(mask_fn,  sprintf('/masks/slice%i', curr_slice) ,'creation_date', datestr(now))
@@ -206,17 +212,26 @@ h5writeatt(mask_fn,  sprintf('/masks'), 'session', session);
 h5writeatt(mask_fn,  sprintf('/masks'), 'acquisition', acquisition);
 h5writeatt(mask_fn,  sprintf('/masks'), 'run', run);
 h5writeatt(mask_fn,  sprintf('/masks'), 'zproj', zproj_type);
+h5writeatt(mask_fn,  sprintf('/masks'), 'ref_file', ref_file);
+h5writeatt(mask_fn,  sprintf('/masks'), 'ref_channel', ref_channel);
 
 % h5disp(mask_fn)
-info = hdf5info(mask_fn)
+info = hdf5info(mask_fn);
 
 %
 for a=1:length(info.GroupHierarchy.Groups(1).Attributes)
     roiset_attr = info.GroupHierarchy.Groups(1).Attributes;
-    fprintf('%s: %s\n', roiset_attr(a).Name, roiset_attr(a).Value.Data);
+    if isnumeric(roiset_attr(a).Value)
+        fprintf('%s: %i\n', roiset_attr(a).Name, roiset_attr(a).Value);
+    else
+        fprintf('%s: %s\n', roiset_attr(a).Name, roiset_attr(a).Value.Data);
+    end
 end
 
-    
+%% Clean up tmp file
+
+
+
 %%
 % 
 % %save roiparams
