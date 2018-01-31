eac3e62476dd999ad262225a1bcfd431b98a69a4
Juliana Rhee <juliana.rhee@gmail.com>
2018-01-31 14:14:17 -0500

    add option to shell args for PID, in case multiple tmp_pids exist

--- pipeline/python/slurm/batch_process_pids.sh
+++ pipeline/python/slurm/batch_process_pids.sh
@@ -6,17 +6,25 @@ if [ "$#" -gt 2 ]; then
     ACQUISITION="$3"
     RUN="$4"
     echo "Processing SINGLE run..."
+    if [ "$#" -gt 4 ]; then
+        PIDHASH="$5"
+        echo "Requested single PID to process."
+    else
+        PIDHASH=""
+    fi
     PIDPATH="/n/coxfs01/2p-data/${ANIMALID}/${SESSION}/${ACQUISITION}/${RUN}/processed/tmp_pids"
 else
     echo "Processing specified runs in session..."
-    PIDPATH="/n/coxfs01/2p-data/${ANIMALID}/${SESSION}/tmp_spids"  
+    PIDPATH="/n/coxfs01/2p-data/${ANIMALID}/${SESSION}/tmp_spids"
+    PIDHASH=""
 fi
 #SPATH="/n/coxfs01/2p-data/${ANIMALID}/${SESSION}/tmp_spids" 
 #SPATH="/nas/volume1/2photon/data/${ANIMALID}/${SESSION}/tmp_spids"
 echo $PIDPATH
 
 #export PYTHONBUFFERED=1
-FILES=($PIDPATH/*.json)
+FILES=($PIDPATH/*$PIDHASH.json)
+echo "PID LIST: $FILES"
 
 # get size of array
 NUMFILES=${#FILES[@]}
@@ -28,7 +36,7 @@ echo "N files: $ZBNUMFILES"
 if [ $ZBNUMFILES -ge 0 ]; then
     # export filenames individually
     for i in ${!FILES[*]};do export FILES_$i="${FILES[$i]}";done	
-    
+     
     # submit to slurm
     sbatch --array=0-$ZBNUMFILES /n/coxfs01/2p-pipeline/repos/2p-pipeline/pipeline/python/slurm/process_runs.sbatch    
 fi
