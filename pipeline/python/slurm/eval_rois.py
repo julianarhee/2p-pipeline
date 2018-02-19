1ad8dcd6a15b1beb50364fe3150c04f386a4c50b
julianarhee <juliana.rhee@gmail.com>
2018-02-18 22:26:17 -0500

    include rootdir for root-checking for slurm scripts

--- pipeline/python/slurm/eval_rois.py
+++ pipeline/python/slurm/eval_rois.py
@@ -12,7 +12,7 @@ import logging
 from pipeline.python.evaluation.evaluate_roi_extraction import run_rid_eval
 
 def main():
-    
+
     rid_path = sys.argv[1]
     nproc = sys.argv[2]
     #cluster_backend = sys.argv[3]
@@ -21,27 +21,27 @@ def main():
     else:
         nproc = int(nproc)
 #    if len(cluster_backend) == 0:
-#        cluster_backend = 'local' 
-#        
+#        cluster_backend = 'local'
+#
     roi_hash = os.path.splitext(os.path.split(rid_path)[-1])[0].split('_')[-1]
     logdir = os.path.join(os.path.split(rid_path)[0], 'logging_%s' % roi_hash)
     if not os.path.exists(logdir):
         os.makedirs(logdir)
     print "Logging to dir: %s" % logdir
     #print "Cluster backend: %s" % cluster_backend
- 
+
     logging.basicConfig(level=logging.DEBUG, filename="%s/logfile_%s_roieval" % (logdir, roi_hash), filemode="a+",
                         format="%(asctime)-15s %(levelname)-8s %(message)s")
-    
-    logging.info("RID %s -- starting memmapping ..." % roi_hash) 
+
+    logging.info("RID %s -- starting memmapping ..." % roi_hash)
     logging.info(rid_path)
-    
-    eval_filepath = run_rid_eval(rid_path, nprocs=nproc, cluster_backend='local')
-    
+
+    eval_filepath = run_rid_eval(rid_path, nprocs=nproc, cluster_backend='local', rootdir='/n/coxfs01/2p-data')
+
     logging.info("FINISHED evaluating ROIs from RID:\n%s" % roi_hash)
     logging.info("Saved eval results to: %s" % eval_filepath)
 
 if __name__=="__main__":
-     
+
     main()
-    
+
