b766b58b4fd6a96598b453ac9061cd9a263f8263
julianarhee <juliana.rhee@gmail.com>
2018-04-17 16:52:18 -0400

    fix bool assignment for is_3D

--- pipeline/matlab/rois/get_rois_manual2D.m
+++ pipeline/matlab/rois/get_rois_manual2D.m
@@ -9,7 +9,7 @@ clc; clear all;
 
 rootdir = '/mnt/odyssey'
 animalid = 'CE077' %'JR063';
-session = '20180326' %'20171202_JR063';
+session = '20180413' %'20171202_JR063';
 roi_id = 'rois001' %'e4893c';
 
 %% Load RID parameter set:
@@ -88,9 +88,9 @@ else
 end
     
 if length(runinfo.slices) > 1
-    is_3D = false;
+    is_3D = true
 else
-    is_3D = true;
+    is_3D = false
 end
 
 %% Get AVERAGE SLICE files:
