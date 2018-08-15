4ef12aabb8261ff060f16a95088390a445c869dc
Juliana Rhee <juliana.rhee@gmail.com>
2018-08-15 19:40:11 -0400

    delete skipped trials from trialevents dict and  overwrite parsed mw trials

--- pipeline/python/paradigm/extract_stimulus_events.py
+++ pipeline/python/paradigm/extract_stimulus_events.py
@@ -192,20 +192,25 @@ def extract_frames_to_trials(serialfn_path, mwtrial_path, runinfo, blank_start=T
     
 # all_bitcodes[58390:58420]
     #%%
-    #use_loop = True# False
-#    trial = 'trial00287'
-#    tidx = 286
+#    trial = tmp_frames['last_trial'] #'trial00287'
+#    tidx = int(trial[5:]) - 1
 #    curr_frames = tmp_frames['starting_frame_set']
-#    prev_trial = 'trial00286' #'trial00001'
-    prev_trial = 'trial00001'
+#    prev_trial = 'trial%05d' % (int(trial[5:])-1) #'trial00286' #'trial00001'
+#    frame_bitcodes = tmp_frames['frame_bitcodes']
+#    modes_by_frame = tmp_frames['modes_by_frame']
+#    mwtrial_path = tmp_frames['mwtrial_path'].replace('/n/coxfs01', '/Volumes/coxfs01')
+#    with open(mwtrial_path, 'r') as f: mwtrials = json.load(f)
+#    first_frame = '%i_p0' % int(curr_frames[0].split('_')[0])
+#    
+    #prev_trial = 'trial00001'
     skip_trial = False
     trial_frames = []
     ntrials = len(mwtrials.keys())
     durs = []
     skipped = {}
-    for tidx, trial in enumerate(sorted(mwtrials.keys(), key=natural_keys)): #[0:254]: #[0:46]):
+    #for tidx, trial in enumerate(sorted(mwtrials.keys(), key=natural_keys)): #[0:254]: #[0:46]):
     
-    #for tidx, trial in zip(np.arange(286, len(mwtrials.keys())), sorted(mwtrials.keys(), key=natural_keys)[286:]):
+    for tidx, trial in zip(np.arange(tidx, len(mwtrials.keys())), sorted(mwtrials.keys(), key=natural_keys)[tidx:]):
     
         print "- - parsing %s" % trial
         # Create hash of current MWTRIAL dict:
@@ -221,8 +226,8 @@ def extract_frames_to_trials(serialfn_path, mwtrial_path, runinfo, blank_start=T
         # First, check if first "stimulus" bit-code is actually an image (in case frame-trigger  update missed at start)
         minframes = 5 #4 #5 #4
         
-#        if trial == 'trial00289':
-#            break
+        if trial == 'trial00041':
+            break
         
         if skip_trial: # Skip the PREVIOUS trials, so jump full trial's worth of frame indices:
             print "... Skipping previous trials' frames (%s)..." % prev_trial
@@ -370,7 +375,15 @@ def extract_frames_to_trials(serialfn_path, mwtrial_path, runinfo, blank_start=T
     if len(rounded_durs) != len(unique_mw_stim_durs):
         print " *** WARNING -- funky stim durs found:", rounded_durs
         print " --- found MW stim durs:", unique_mw_stim_durs
-        
+    
+    # REmove skipped:
+    for hkey,nkey in skipped.items():
+        del trialevents[hkey]
+        del mwtrials[nkey]
+        with open(mwtrial_path, 'w') as f:
+            json.dump(mwtrials)
+        print "Updated MWtrials and trial events."
+        print "TOTAL N trials:", len(mwtrials.keys())
 #%%
     return trialevents
 
