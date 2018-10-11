96ce3904dd1e74d8a6492a4940f117cbddbd3c5f
Juliana Rhee <juliana.rhee@gmail.com>
2018-10-11 18:17:39 -0400

    add post ITI dur as arg for slurmified trace extraction

--- pipeline/python/slurm/extract_traces.sh
+++ pipeline/python/slurm/extract_traces.sh
@@ -2,4 +2,4 @@
 mkdir ${1}_${2}_${3}_${4}_${5}_out
 cd ${1}_${2}_${3}_${4}_${5}_out
 
-sbatch -o ${3}_${4}_${5}.out -e ${3}_${4}_${5}.err /n/coxfs01/2p-pipeline/repos/2p-pipeline/pipeline/python/slurm/extract_traces.sbatch ${1} ${2} ${3} ${4} ${5} ${6} ${7}
+sbatch -o ${3}_${4}_${5}.out -e ${3}_${4}_${5}.err /n/coxfs01/2p-pipeline/repos/2p-pipeline/pipeline/python/slurm/extract_traces.sbatch ${1} ${2} ${3} ${4} ${5} ${6} ${7} ${8}
