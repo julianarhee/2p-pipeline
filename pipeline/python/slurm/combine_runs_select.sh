6ed96d9fa2a071d9782b0d727171f60d15806208
Juliana Rhee <juliana.rhee@gmail.com>
2019-05-28 13:56:43 -0400

    log files, don't need dirs

--- pipeline/python/slurm/combine_runs_select.sh
+++ pipeline/python/slurm/combine_runs_select.sh
@@ -1,5 +1,5 @@
 #
-mkdir ${1}_${2}_${3}_${4}_${5}_out
-cd ${1}_${2}_${3}_${4}_${5}_out
+#mkdir ${1}_${2}_${3}_${4}_${5}_out
+#cd ${1}_${2}_${3}_${4}_${5}_out
 
-sbatch -o ${3}_${4}_${5}.out -e ${3}_${4}_${5}.err /n/coxfs01/2p-pipeline/repos/2p-pipeline/pipeline/python/slurm/combine_runs_select.sbatch ${1} ${2} ${3} ${4} ${5} ${6} ${7} ${8} ${9}
+sbatch -o combine_select_${1}_${2}_${3}_${4}_${5}.out -e combine_select_${1}_${2}_${3}_${4}_${5}.err /n/coxfs01/2p-pipeline/repos/2p-pipeline/pipeline/python/slurm/combine_runs_select.sbatch ${1} ${2} ${3} ${4} ${5} ${6}
--- pipeline/python/slurm/universal_std.sh
+++ pipeline/python/slurm/universal_std.sh
@@ -1,5 +1,6 @@
-#
-mkdir ${1}_${2}_${3}_${4}_STD_out
-cd ${1}_${2}_${3}_${4}_STD_out
+# create logging dir + run command
 
-sbatch -o ${2}_${3}_${4}.out -e ${2}_${3}_${4}.err /n/coxfs01/2p-pipeline/repos/2p-pipeline/pipeline/python/slurm/universal_std.sbatch ${1} ${2} ${3} ${4}
+#mkdir ${1}_${2}_${3}_${4}_STD_out
+#cd ${1}_${2}_${3}_${4}_STD_out
+
+sbatch -o activitymap_${1}_${2}_${3}_${4}.out -e activitymap_${1}_{2}_${3}_${4}.err /n/coxfs01/2p-pipeline/repos/2p-pipeline/pipeline/python/slurm/universal_std.sbatch ${1} ${2} ${3} ${4}
