9d175de0c57a8f21b3e7e9905d2788b6cee85a33
Juliana Rhee <juliana.rhee@gmail.com>
2019-06-07 19:49:11 -0400

    add opt for creating activity map from subset of runs

--- pipeline/python/slurm/universal_std.sh
+++ pipeline/python/slurm/universal_std.sh
@@ -3,4 +3,4 @@
 #mkdir ${1}_${2}_${3}_${4}_STD_out
 #cd ${1}_${2}_${3}_${4}_STD_out
 
-sbatch -o activitymap_${1}_${2}_${3}_${4}.out -e activitymap_${1}_{2}_${3}_${4}.err /n/coxfs01/2p-pipeline/repos/2p-pipeline/pipeline/python/slurm/universal_std.sbatch ${1} ${2} ${3} ${4}
+sbatch -o activitymap_${1}_${2}_${3}_${4}.out -e activitymap_${1}_${2}_${3}_${4}.err /n/coxfs01/2p-pipeline/repos/2p-pipeline/pipeline/python/slurm/universal_std.sbatch ${1} ${2} ${3} ${4}
