8385958c77178f077bf679f261f9ead0d4643072
Juliana Rhee <juliana.rhee@gmail.com>
2019-06-28 11:57:40 -0400

    show cropped colorbar for retino position phase maps

--- pipeline/python/retinotopy/segment_retinotopy.py
+++ pipeline/python/retinotopy/segment_retinotopy.py
@@ -24,20 +24,23 @@ from skimage.measure import block_reduce
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 from pipeline.python.retinotopy import utils as rutils
+from pipeline.python.utils import natural_keys, label_figure
 
 
 #%%
 
 
-def filter_map_by_magratio(magratio, phase, cond='right', mag_thr=None, mag_perc=0.05):
+def filter_map_by_magratio(magratio, phase, dims=(512, 512), ds_factor=2, cond='right', mag_thr=None, mag_perc=0.05):
     if mag_thr is None:
         mag_thr = magratio.max().max()*mag_perc
         
     currmags = magratio[trials_by_cond[cond]]
     currmags[currmags<mag_thr] = np.nan
     currmags_mean = np.nanmean(currmags, axis=1)
-    d1 = int(np.sqrt(currmags_mean.shape[0]))
-    currmags_map = np.reshape(currmags_mean, (d1, d1))
+    #d1 = int(np.sqrt(currmags_mean.shape[0]))
+    d1 = dims[0] / ds_factor
+    d2 = dims[1] / ds_factor
+    currmags_map = np.reshape(currmags_mean, (d1, d2))
     
 
     currphase = phase[trials_by_cond[cond]]
@@ -45,7 +48,7 @@ def filter_map_by_magratio(magratio, phase, cond='right', mag_thr=None, mag_perc
     currphase_mean_c = rutils.correct_phase_wrap(currphase_mean)
     
     currphase_mean_c[np.isnan(currmags_mean)] = np.nan
-    currphase_map_c = np.reshape(currphase_mean_c, (d1, d1))
+    currphase_map_c = np.reshape(currphase_mean_c, (d1, d2))
     
     return currmags_map, currphase_map_c, mag_thr
 
@@ -71,10 +74,10 @@ def plot_filtered_maps(cond, currmags_map, currphase_map_c, mag_thr):
 #%%
 
 rootdir = '/n/coxfs01/2p-data'
-animalid = 'JC084' #'JC059'
-session = '20190522' #'20190227'
-fov = 'FOV1_zoom2p0x' #'FOV4_zoom4p0x'
-run = 'retino_run2'
+animalid = 'JC091' #'JC059'
+session = '20190623' #'20190227'
+fov = 'FOV1_zoom1p0x' #'FOV4_zoom4p0x'
+run = 'retino_run1'
 traceid = 'analysis001' #'traces001'
 visual_area = ''
 
@@ -121,29 +124,7 @@ stiminfo, trials_by_cond = rutils.get_retino_stimulus_info(mwinfo, runinfo)
 #stiminfo['trials_by_cond'] = trials_by_cond
 print "Trials by condN:", trials_by_cond
 
-#%%
 
-fit, magratio, phase, trials_by_cond = rutils.trials_to_dataframes(processed_fpaths, conditions_fpath)
-
-
-#%%
-
-mag_thr = 0.005
-
-magmaps = {}
-phasemaps = {}
-magthrs = {}
-for cond in trials_by_cond.keys():    
-    magmaps[cond], phasemaps[cond], magthrs[cond] = filter_map_by_magratio(magratio, phase, cond=cond, mag_thr=mag_thr)
-    fig = plot_filtered_maps(cond, magmaps[cond], phasemaps[cond], magthrs[cond])
-
-
-absolute_az = (phasemaps['left'] - phasemaps['right']) / 2.
-delay_az = (phasemaps['left'] + phasemaps['right']) / 2.
-
-
-absolute_el = (phasemaps['bottom'] - phasemaps['top']) / 2.
-delay_el = (phasemaps['bottom'] + phasemaps['top']) / 2.
 #%%
 
 
@@ -154,11 +135,37 @@ screen_right = screen['azimuth']/2.
 screen_top = screen['elevation']/2.
 screen_bottom = -1*screen['elevation']/2.
     
-top_cutoff = screen_top / screen_right
-bottom_cutoff = screen_bottom / screen_left
+elev_cutoff = screen_top / screen_right
 
 
 
+
+#%%
+
+
+fit, magratio, phase, trials_by_cond = rutils.trials_to_dataframes(processed_fpaths, conditions_fpath)
+
+
+#%%
+
+mag_thr = 0.003
+d2 = runinfo['pixels_per_line']
+d1 = runinfo['lines_per_frame']
+
+magmaps = {}
+phasemaps = {}
+magthrs = {}
+for cond in trials_by_cond.keys():    
+    magmaps[cond], phasemaps[cond], magthrs[cond] = filter_map_by_magratio(magratio, phase, cond=cond, mag_thr=mag_thr, dims=(d1, d2))
+    fig = plot_filtered_maps(cond, magmaps[cond], phasemaps[cond], magthrs[cond])
+
+
+absolute_az = (phasemaps['left'] - phasemaps['right']) / 2.
+delay_az = (phasemaps['left'] + phasemaps['right']) / 2.
+
+
+absolute_el = (phasemaps['bottom'] - phasemaps['top']) / 2.
+delay_el = (phasemaps['bottom'] + phasemaps['top']) / 2.
 
 #%%
 fig, axes = pl.subplots(2,2)
@@ -179,7 +186,9 @@ cb.outline.set_visible(False)
 
 cbaxes = fig.add_axes(cbar2_axes) 
 cb = pl.colorbar(im2, cax = cbaxes, orientation=cbar2_orientation)
-cb.ax.set_ylim([cb.norm(-np.pi*top_cutoff), cb.norm(np.pi*top_cutoff)])
+#cb.ax.set_ylim([cb.norm(-np.pi*top_cutoff), cb.norm(np.pi*top_cutoff)])
+cb.ax.axhline(y=cb.norm(-np.pi*elev_cutoff), color='w', lw=1)
+cb.ax.axhline(y=cb.norm(np.pi*elev_cutoff), color='w', lw=1)
 cb.ax.axis('off')
 cb.outline.set_visible(False)
 
@@ -187,18 +196,33 @@ pl.subplots_adjust(top=0.8)
 
 for ax in axes.flat:
     ax.axis('off')
+    
+label_figure(fig, data_identifier)
+figname = 'absolute_and_delay_maps_magthr_%.3f' % mag_thr
+pl.savefig(os.path.join(processed_dir, 'figures', '%s.png' % figname))
+
+
+
 
 #%%
 # Load surface image to plot overlay:
 #surface_fpath = glob.glob(os.path.join(rootdir, animalid, 'macro_maps', '*', '*urf*'))[0]
 #surface_img = cv2.imread(surface_fpath, -1)
 #print(surface_img.shape)
-ch_idx = 1
-anats = glob.glob(os.path.join(rootdir, animalid, session, fov, 'anatomical', 'processed',\
-                               'processed*', 'mcorrected_*mean_deinterleaved', 'Channel%02d' % ch_idx, 'File*', '*.tif'))
 
+overlay_surface = False
+if overlay_surface:
+    ch_num = 2
+    fov_imgs = glob.glob(os.path.join(rootdir, animalid, session, fov, 'anatomical', 'processed',\
+                                   'processed*', 'mcorrected_*mean_deinterleaved', 'Channel%02d' % ch_num, 'File*', '*.tif'))
+else:
+    ch_num = 1
+    fov_imgs = glob.glob(os.path.join(run_dir, 'processed', 'processed*', 'mcorrected_*mean_deinterleaved',\
+                                      'Channel%02d' % ch_num, 'File*', '*.tif'))
+    
+    
 imlist = []
-for anat in anats:
+for anat in fov_imgs:
     im = tf.imread(anat)
     imlist.append(im)
 surface_img = np.array(imlist).mean(axis=0)
@@ -212,6 +236,14 @@ if surface_img.shape[0] != absolute_az.shape[0]:
     surface_img = block_reduce(surface_img, (2,2), func=np.mean)
     
 #%%
+
+import matplotlib as mpl
+import matplotlib.cm as cmx
+
+
+vmin = -np.pi
+vmax = np.pi
+
 fig, axes = pl.subplots(1,2)
 ax = axes[0]
 ax.imshow(surface_img, cmap='gray', origin='upper')
@@ -225,22 +257,31 @@ ax.imshow(surface_img, cmap='gray')
 im2 = ax.imshow(absolute_el, cmap='nipy_spectral', vmin=-np.pi, vmax=np.pi, alpha=0.7, origin='upper')
 
 cbar1_orientation='horizontal'
-cbar1_axes = [0.37, 0.7, 0.1, 0.07]
+cbar1_axes = [0.37, 0.65, 0.1, 0.07]
 cbar2_orientation='vertical'
-cbar2_axes = [0.8, 0.7, 0.1, 0.07]
+cbar2_axes = [0.8, 0.65, 0.1, 0.07]
 
-cbaxes = fig.add_axes(cbar1_axes) 
-cb = pl.colorbar(im1, cax = cbaxes, orientation=cbar1_orientation)  
-cb.ax.axis('off')
-cb.outline.set_visible(False)
 
-cbaxes = fig.add_axes(cbar2_axes) 
-cb = pl.colorbar(im2, cax = cbaxes, orientation=cbar2_orientation)
-cb.ax.set_ylim([cb.norm(-np.pi*top_cutoff), cb.norm(np.pi*top_cutoff)])
-cb.ax.axis('off')
-cb.outline.set_visible(False)
+cnorm = mpl.colors.Normalize(vmin=vmin, vmax=vmax)
+scalarmap = cmx.ScalarMappable(norm=cnorm, cmap='nipy_spectral')
+print("scaled cmap lim:", scalarmap.get_clim())
+bounds = np.linspace(vmin, vmax)
+scalarmap.set_array(bounds)
 
-pl.subplots_adjust(top=0.8)
+cbar1_ax = fig.add_axes(cbar1_axes)
+cbar1 = fig.colorbar(im1, cax=cbar1_ax, orientation=cbar1_orientation)
+cbar1.ax.axis('off')
+
+cbar2_ax = fig.add_axes(cbar2_axes)
+cbar2 = fig.colorbar(im2, cax=cbar2_ax, orientation=cbar2_orientation)
+cbar2.ax.axhline(y=cbar2.norm(-np.pi*elev_cutoff), color='w', lw=1)
+cbar2.ax.axhline(y=cbar2.norm(np.pi*elev_cutoff), color='w', lw=1)
+cbar2.ax.axis('off')
+
+cbar1.outline.set_visible(False)
+cbar2.outline.set_visible(False)
+
+#pl.subplots_adjust(top=0.8)
 
 for ax in axes.flat:
     ax.axis('off')
