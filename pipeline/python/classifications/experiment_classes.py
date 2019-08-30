db3084fe2dd06802d9747fc65a7e16f2ffa4f2a4
Juliana Rhee <juliana.rhee@gmail.com>
2019-08-30 16:27:39 -0400

    check if self.data None

--- pipeline/python/classifications/experiment_classes.py
+++ pipeline/python/classifications/experiment_classes.py
@@ -1147,7 +1147,8 @@ class Experiment(object):
         make_equal = kwargs.get('make_equal', True)
         get_grouped = kwargs.get('get_grouped', True)
 
-        if self.data is None or self.data.traces is None or self.trace_type != 'corrected':
+        if self.data is None or len([i for i in dir(self.data) if not i.startswith('__')])==0 \
+            or ('traces' in dir(self.data) and self.data.traces is None) or self.trace_type != 'corrected':
             self.load(trace_type='corrected', make_equal=make_equal)
         
         # if responsive_test is not None:
