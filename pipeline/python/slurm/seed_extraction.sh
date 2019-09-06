22a0ee6945306b953fa9584f72c60472c0c47881
Juliana Rhee <juliana.rhee@gmail.com>
2019-09-06 19:19:14 -0400

    add missing opt for shell wrapper and seeded caiman extractioon

--- pipeline/python/slurm/seed_extraction.sh
+++ pipeline/python/slurm/seed_extraction.sh
@@ -1,2 +1,2 @@
 #
-sbatch -o seed_caiman_${1}_${2}_${3}_${4}_${5}_${6}.out -e seed_caiman_${1}_${2}_${3}_${4}_${5}_${6}.err /n/coxfs01/2p-pipeline/repos/2p-pipeline/pipeline/python/slurm/seed_extraction.sbatch ${1} ${2} ${3} ${4} ${5} ${6}
+sbatch -o seed_caiman_${1}_${2}_${3}_${4}_${5}_${6}.out -e seed_caiman_${1}_${2}_${3}_${4}_${5}_${6}.err /n/coxfs01/2p-pipeline/repos/2p-pipeline/pipeline/python/slurm/seed_extraction.sbatch ${1} ${2} ${3} ${4} ${5} ${6} ${7}
