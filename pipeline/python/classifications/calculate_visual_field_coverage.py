d014b50860592aea1289865d05b6092686fe61ae
Juliana Rhee <juliana.rhee@gmail.com>
2019-10-10 11:56:13 -0400

    update visual field coverage and overlap calculation with updated experiment classes and funcs

--- pipeline/python/classifications/calculate_visual_field_coverage.py
+++ pipeline/python/classifications/calculate_visual_field_coverage.py
@@ -21,7 +21,7 @@ import matplotlib.gridspec as gridspec
 
 from pipeline.python.classifications import experiment_classes as util
 from pipeline.python.classifications import test_responsivity as resp
-from pipeline.python.classifications import run_experiment_stats as rstats
+from pipeline.python.classifications import responsivity_stats as rstats
 from pipeline.python.utils import label_figure, natural_keys, convert_range
 from pipeline.python.retinotopy import convert_coords as coor
 from pipeline.python.retinotopy import fit_2d_rfs as fitrf
@@ -123,7 +123,7 @@ fov = 'FOV1_zoom2p0x'
 
 traceid = 'traces001'
 
-create_new = True
+create_new = False
 trace_type = 'corrected'
 responsive_test = 'ROC'
 convert_um = True
@@ -131,7 +131,7 @@ convert_um = True
 
 #%%
 S = get_session_object(animalid, session, fov, traceid=traceid, trace_type=trace_type,
-                       create_new=True, rootdir=rootdir)
+                       create_new=create_new, rootdir=rootdir)
 
 if S is not None:
     
@@ -145,7 +145,7 @@ if S is not None:
                                                           responsive_test=responsive_test, 
                                                           traceid=traceid, trace_type=trace_type,
                                                           create_new=False, rootdir=rootdir,
-                                                          pretty_plots=False)
+                                                          pretty_plots=False, update_self=True)
     
     
     # Create output dir for figures:
@@ -199,11 +199,38 @@ stimsizes = S.get_stimulus_sizes()
 
 # Draw RFs
 #rf_fits_df = gdfs[rf_exp_name].fits
-xres = list(set(np.diff(gdfs[rf_exp_name].finfo['col_vals'])))[0]
-yres = list(set(np.diff(gdfs[rf_exp_name].finfo['row_vals'])))[0]
-sigma_scale = 2.36
+xres = list(set(np.diff(gdfs[rf_exp_name].fitinfo['col_vals'])))[0]
+yres = list(set(np.diff(gdfs[rf_exp_name].fitinfo['row_vals'])))[0]
+#sigma_scale = 2.36
 
 print("X- and Y-res: (%i, %i)" % (xres, yres))
+
+# Get size range of stimuli shown:
+gratings_sz = min(stimsizes['gratings'])
+blobs_sz_min = min(stimsizes['blobs'])
+blobs_sz_max = max(stimsizes['blobs'])
+
+print("Gratings - apertured size: %i" % gratings_sz)
+print("Blobs - min/max size: (%i, %i)" % (blobs_sz_min, blobs_sz_max))
+
+
+
+
+#%% GET FOV info
+
+from pipeline.python.retinotopy import convert_coords as coords
+masks, zimg = S.load_masks(rois='rois001')
+print(masks.shape)
+#roi_contours = coords.contours_from_masks(masks)
+
+rf_rois = gdfs[rf_exp_name].fits.index.tolist() 
+fovinfo = coords.get_roi_fov_info(masks, zimg, roi_list=rf_rois)
+#fov_pos_x, rf_xpos, xlim, fov_pox_y, rf_ypos, ylim = coords.get_roi_position_um(roi_contours,
+#                                                                         rf_exp_name=rf_exp_name,
+#                                                                         convert_um=True)
+
+pl.figure()
+pl.scatter(gdfs[rf_exp_name].fits['x0'], fovinfo['positions']['ml_pos'])
 
 
 # In[29]:
@@ -267,7 +294,7 @@ in_blobs_max = []
 rf_dist_from_center = []
 rf_avg_size = []
 for roi in sorted(gdfs[rf_exp_name].rois):
-    sx, sy, th, x0, y0 = gdfs[rf_exp_name].fits.loc[roi]
+    _, sx, sy, th, x0, y0 = gdfs[rf_exp_name].fits.loc[roi]
     s_ell = create_ellipse((x0, y0), (abs(sx)*sigma_scale/2., abs(sy)*sigma_scale/2.), np.rad2deg(th))
     v_ell = np.array(s_ell.exterior.coords.xy)
     p_ell = Polygon(v_ell.T, edgecolor='k', alpha=0.5, facecolor='none', lw=0.2)
@@ -284,7 +311,7 @@ for roi in sorted(gdfs[rf_exp_name].rois):
     
     ## get distance bw RF centers and stimulus location:
     rf_dist_from_center.append(np.sqrt((x0 - xpos)**2 + (y0 - ypos)**2))
-    rf_avg_size.append(np.mean([abs(sx)*sigma_scale/2., abs(sy)*sigma_scale/2.]))
+    rf_avg_size.append(np.mean([abs(sx)*sigma_scale/1., abs(sy)*sigma_scale/1.]))
     
 
 # Draw patches:
@@ -314,7 +341,7 @@ ax = sns.boxplot(x="stimulus", y="overlap", data=overlap_df, ax=ax,
                  palette=colordict, saturation=1.0, notch=True, boxprops=dict(alpha=.5))
 ax.set_xlabel('')
 ax.set_ylabel('Overlap area\n(% of RF)', fontsize=8)
-ax.set_ylim([0, 100])
+#ax.set_ylim([0, 100])
 ax.tick_params(axis='x', which='both', length=0, labelsize=8)
 ax.tick_params(axis='y', which='both', length=0, labelsize=8)
 
@@ -329,8 +356,8 @@ ax2a.set_xlim([0, max(rf_avg_size)+10])
 ax2a.set_xlabel('Average RF size\n(deg)', fontsize=8)
 ax2a.set_ylabel('kde', fontsize=8)
 ax2a.tick_params(axis='both', which='both', length=3, labelsize=8)
-ax2a.yaxis.set_major_locator(MaxNLocator(2))
-ax2a.yaxis.set_major_formatter(FormatStrFormatter('%.1f'))
+#ax2a.yaxis.set_major_locator(MaxNLocator(2))
+ax2a.yaxis.set_major_formatter(FormatStrFormatter('%.2f'))
 sns.despine(trim=True, offset=4, ax=ax2a)
 
 # ---- Distance from stimulus center -----------------------
@@ -341,35 +368,36 @@ ax2b.set_ylabel('kde', fontsize=8)
 ax2b.tick_params(axis='both', which='both', length=3, labelsize=8)
 ymax = max([ax2a.get_ylim()[-1], ax2b.get_ylim()[-1]])
 ax2b.set_ylim([0, ymax])
-ax2b.yaxis.set_major_locator(MaxNLocator(2))
-ax2b.yaxis.set_major_formatter(FormatStrFormatter('%.1f'))
+#ax2b.yaxis.set_major_locator(MaxNLocator(2))
+ax2b.yaxis.set_major_formatter(FormatStrFormatter('%.2f'))
 sns.despine(trim=True, offset=4, ax=ax2b)
 
 
 # ---- Spatially sorted ROIs vs. RF position -----------------------
-rf_rois = gdfs[rf_exp_name].fits.index.tolist() 
 colors = ['k' for _ in range(len(rf_rois))]
 
 # Get values for azimuth/elevation:
-fov_pos_x, rf_xpos, xlim, fov_pox_y, rf_ypos, ylim = get_roi_position_um(tmp_roi_contours,
-                                                                         rf_exp_name=rf_exp_name,
-                                                                         convert_um=True)
+units = 'um'
 
+ctx_ap = fovinfo['positions']['ap_pos']
+ctx_ml = fovinfo['positions']['ml_pos']
+rf_ap = gdfs[rf_exp_name].fits['y0']
+rf_ml = gdfs[rf_exp_name].fits['x0']
 
 ax3a = pl.subplot2grid((3, 4), (1, 0), colspan=1, rowspan=1)
-ax3a.scatter(fov_pos_y, rf_xpos, c=colors, alpha=0.3) # FOV y-axis is left-right on brain
+ax3a.scatter(ctx_ml, rf_ml, c=colors, alpha=0.3) # FOV y-axis is left-right on brain
 #ax3a.set_title('Azimuth', fontsize=8)
 ax3a.set_ylabel('Azimuth\n(rel. deg.)', fontsize=8)
 ax3a.set_xlabel('FOV position\n(%s)' % units, fontsize=8)
-ax3a.set_xlim([0, ylim])
+#ax3a.set_xlim([0, ylim])
 ax3a.yaxis.set_major_locator(MaxNLocator(5))
 sns.despine(trim=True, offset=4, ax=ax3a)
 
 ax3b = pl.subplot2grid((3, 4), (1, 1), colspan=1, rowspan=1)
-ax3b.scatter(fov_pos_x, rf_ypos, c=colors, alpha=0.3) # FOV x-axis is posterior-anterior on brain
+ax3b.scatter(ctx_ap, rf_ap, c=colors, alpha=0.3) # FOV x-axis is posterior-anterior on brain
 ax3b.set_ylabel('Elevation\n(rel. deg.)', fontsize=8)
 ax3b.set_xlabel('FOV position\n(%s)' % units, fontsize=8)
-ax3b.set_xlim([0, xlim])
+#ax3b.set_xlim([0, xlim])
 #ax.set_aspect('auto')
 ax3b.yaxis.set_major_locator(MaxNLocator(5))
 sns.despine(trim=True, offset=4, ax=ax3b)
@@ -387,8 +415,8 @@ bbox = ax0.get_position()
 print(bbox)
 bbox2 = [bbox.x0 - 0.04, bbox.y0+0.0,  bbox.width-0.04, bbox.height+0.05] 
 ax0.set_position(bbox2) # set a new position
-ax0.legend(fontsize=6, ncol=3, loc='lower center', bbox_to_anchor=[0.5, -0.25])
-
+#ax0.legend(fontsize=6, ncol=3, loc='lower center', bbox_to_anchor=[0.5, -0.25])
+#%%
 label_figure(fig, data_identifier)
 
 pl.savefig(os.path.join(statsfigdir, 'visual_field_coverage_w%s_%s.png' % (rf_exp_name, units)))
