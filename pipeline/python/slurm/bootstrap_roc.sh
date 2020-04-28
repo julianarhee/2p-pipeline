33e75c77cdf4fdba538d24c9ea1c74c18350a457
Juliana Rhee <juliana.rhee@gmail.com>
2020-04-28 12:21:39 -0400

    change log filenames

--- pipeline/python/slurm/bootstrap_roc.sh
+++ pipeline/python/slurm/bootstrap_roc.sh
@@ -1,6 +1,2 @@
-# create logging dir + run command
 
-#mkdir ${1}_${2}_${3}_${4}_STD_out
-#cd ${1}_${2}_${3}_${4}_STD_out
-
-sbatch -o bootstrap_roc_${1}_${2}_${3}_${4}_${5}.out -e bootstrap_roc_${1}_${2}_${3}_${4}_${5}.err /n/coxfs01/2p-pipeline/repos/2p-pipeline/pipeline/python/slurm/bootstrap_roc.sbatch ${1} ${2} ${3} ${4} ${5}
+sbatch -o boot-ROC_${1}-${2}-${3}-${4}-${5}.out -e boot-ROC_${1}-${2}-${3}-${4}-${5}.err /n/coxfs01/2p-pipeline/repos/2p-pipeline/pipeline/python/slurm/bootstrap_roc.sbatch ${1} ${2} ${3} ${4} ${5}
