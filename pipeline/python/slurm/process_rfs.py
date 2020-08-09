a398f08b680fcc4b232954d64210481c5e18be68
Juliana Rhee <juliana.rhee@gmail.com>
2020-08-09 18:54:13 -0400

    run all fovs in parallel, request slurm jobs on all

--- pipeline/python/slurm/process_rfs.py
+++ pipeline/python/slurm/process_rfs.py
@@ -6,6 +6,7 @@ import sys
 import os
 import commands
 import json
+import glob
 import pandas as pd
 import cPickle as pkl
 
@@ -39,6 +40,11 @@ if not os.path.exists('log'):
 
 sys.stdout = open('log/loginfo_RFs.txt', 'w')
 
+old_logs = glob.glob(os.path.join('log', '*.err'))
+old_logs.extend(glob.glob(os.path.join('log', '*.out')))
+for r in old_logs:
+    os.remove(r)
+
 #####################################################################
 #                          find XID files                           #
 #####################################################################
@@ -56,7 +62,7 @@ def load_metadata(rootdir='/n/coxfs01/2p-data', experiment='', traceid='traces00
    
     meta_list=[]
     for (animalid, session, fov), g in sdata.groupby(['animalid', 'session', 'fov']):
-        exp_list = g['experiment'].values
+        exp_list = [e for e in g['experiment'].values if experiment in e]
         for e in exp_list:
             if experiment in e:
                 meta_list.append(tuple(animalid, session, fov, e, traceid))    
@@ -64,16 +70,18 @@ def load_metadata(rootdir='/n/coxfs01/2p-data', experiment='', traceid='traces00
     return meta_list
 
 
-meta_list = [('JC084', '20190522', 'FOV1_zoom2p0x', 'rfs', 'traces001'),
-             ('JC084', '20190525', 'FOV1_zoom2p0x', 'rfs', 'traces001')]
+#meta_list = [('JC084', '20190522', 'FOV1_zoom2p0x', 'rfs', 'traces001'),
+#             ('JC084', '20190525', 'FOV1_zoom2p0x', 'rfs', 'traces001')]
+
+meta_list = load_metadata(experiment='rfs')
 
 if len(meta_list)==0:
     fatal("NO FOVs found.")
 
 
 info("Found %i datasets to process." % len(meta_list))
-for mi, mtup in enumerate(meta_list):
-    info("... %s" % '|'.join(m))
+#for mi, meta in enumerate(meta_list):
+#    info("... %s" % '|'.join(meta))
 
 ################################################################################
 #                               run the pipeline                               #
@@ -85,10 +93,10 @@ for mi, mtup in enumerate(meta_list):
 
 pid_jobids = {}
 for (animalid, session, fov, experiment, traceid) in meta_list:
-    mtag = '-'.join([animalid, session, fov, experiment])
-    cmd = "sbatch --job-name=rfs.{PROCID}.{MTAG} \
-		-o 'log/rfs.{PROCID}.{MTAG}.out' \
-		-e 'log/rfs{PROCID}.{MTAG}.err' \
+    mtag = '-'.join([session, animalid, fov, experiment])
+    cmd = "sbatch --job-name=rfs.{MTAG}.{PROCID} \
+		-o 'log/rfs.{MTAG}.{PROCID}.out' \
+		-e 'log/rfs.{MTAG}.{PROCID}.err' \
 		/n/coxfs01/2p-pipeline/repos/2p-pipeline/pipeline/python/slurm/process_rfs.sbatch \
 		{ANIMALID} {SESSION} {FOV} {EXP} {TRACEID}".format(
                         PROCID=piper, MTAG=mtag, ANIMALID=animalid,
@@ -96,8 +104,8 @@ for (animalid, session, fov, experiment, traceid) in meta_list:
     #info("Submitting PROCESSPID job with CMD:\n%s" % cmd)
     status, joboutput = commands.getstatusoutput(cmd)
     jobnum = joboutput.split(' ')[-1]
-    pid_jobids[phash] = jobnum
-    info("jobids [%s]: %s" % (jobnum, mtag))
+    #pid_jobids[phash] = jobnum
+    info("[%s]: %s" % (jobnum, mtag))
 
 
 info("****done!****")
