ac9f9dc2a0ccaadae2322745ca516fc1e307f0e4
Juliana Rhee <juliana.rhee@gmail.com>
2020-11-24 18:54:07 -0500

    add scripts to run on cluster

--- pipeline/python/slurm/decode_by_ncells.py
+++ pipeline/python/slurm/decode_by_ncells.py
@@ -23,19 +23,19 @@ parser.add_argument('-t', '--traceid', dest='traceid', action='store', default=N
 parser.add_argument('-R', '--resp-test', dest='responsive_test', action='store', default='nstds', help='Responsive test (default=nstds, options: ROC, nstds, None)')
 parser.add_argument('-o', '--overlap', dest='overlap_thr', action='store', default=0.5, help='Overlap threshold (default=0.5)')
 
-parser.add_argument('-x', '--analysis', dest='analysis_type', action='store', default='by_ncells', help='Analysis type (options: by_ncells, single_cells. default=by_ncells')
+parser.add_argument('-X', '--analysis', dest='analysis_type', action='store', default='by_ncells', help='Analysis type (options: by_ncells, single_cells. default=by_ncells')
 
 
 parser.add_argument('-C', '--cvalue', dest='c_value', action='store', default=1.0, help='C value (default=1, set None to tune)')
 
 
 parser.add_argument('-v', '--area', dest='visual_area', action='store', default=None, help='Visual area to process (default, all)')
-
-
-
+parser.add_argument('-k', '--datakeys', nargs='*', dest='included_datakeys', action='append', help='Use like: -k DKEY DKEY DKEY')
+parser.add_argument('--match', dest='match_distns', action='store_true', default=False, help='Set if match distns (only if --analysis=by_ncells)')
 
 
 args = parser.parse_args()
+
 
 def info(info_str):
     sys.stdout.write("INFO:  %s \n" % info_str)
@@ -69,13 +69,13 @@ EXPERIMENT = args.experiment_type
 email = args.email
 
 visual_area = None if args.visual_area in ['None', None] else args.visual_area
-
 traceid = args.traceid
 responsive_test = args.responsive_test
-overlap_thr = float(args.overlap_thr)
+overlap_thr = None if args.overlap_thr in ['None', None] else float(args.overlap_thr)
 
 analysis_type = args.analysis_type
 c_value = None if args.c_value in ['None', None] else float(args.c_value)
+match_distns = args.match_distns
 
 
 # Create a (hopefully) unique prefix for the names of all jobs in this 
@@ -83,7 +83,11 @@ c_value = None if args.c_value in ['None', None] else float(args.c_value)
 # identified unambiguously
 piper = uuid.uuid4()
 piper = str(piper)[0:4]
-logdir = 'LOG__%s_%s_%s_overlap%i' % (analysis_type, str(visual_area), EXPERIMENT, int(overlap_thr*10)) 
+match_str = 'matchdistns_' if match_distns else ''
+if overlap_thr is None:
+    logdir = 'LOG__%s%s_%s_%s_no-rfs' % (match_str, analysis_type, str(visual_area), EXPERIMENT) 
+else:
+    logdir = 'LOG__%s%s_%s_%s_overlap-%i' % (match_str, analysis_type, str(visual_area), EXPERIMENT, int(overlap_thr*10)) 
 if not os.path.exists(logdir):
     os.mkdir(logdir)
 
@@ -138,43 +142,90 @@ if analysis_type=='by_ncells':
         for ncells in ncells_test:
             mtag = '%s_%s_%i' % (visual_area, C_str, ncells) 
             #
-            cmd = "sbatch --job-name={PROCID}.{ANALYSIS}.{MTAG} \
+            if match_distns:
+                cmd = "sbatch --job-name={PROCID}.{ANALYSIS}.{MTAG} \
                 -o '{LOGDIR}/{PROCID}.{ANALYSIS}.{MTAG}.out' \
                 -e '{LOGDIR}/{PROCID}.{ANALYSIS}.{MTAG}.err' \
-                /n/coxfs01/2p-pipeline/repos/2p-pipeline/pipeline/python/slurm/decode_by_ncells.sbatch \
+    /n/coxfs01/2p-pipeline/repos/2p-pipeline/pipeline/python/slurm/decode_by_ncells_match.sbatch \
         {EXP} {TRACEID} {RTEST} {OVERLAP} {ANALYSIS} {CVAL} {VAREA} {NCELLS} {DKEY}".format(
-                PROCID=piper, MTAG=mtag, LOGDIR=logdir,
-                EXP=EXPERIMENT, TRACEID=traceid, ANALYSIS=analysis_type,
-                RTEST=responsive_test, OVERLAP=overlap_thr, 
-                CVAL=c_value, VAREA=visual_area, NCELLS=ncells, DKEY=datakey) 
-            #
+                    PROCID=piper, MTAG=mtag, LOGDIR=logdir,
+                    EXP=EXPERIMENT, TRACEID=traceid, ANALYSIS=analysis_type,
+                    RTEST=responsive_test, OVERLAP=overlap_thr, 
+                    CVAL=c_value, VAREA=visual_area, NCELLS=ncells, DKEY=datakey) 
+ 
+            else:
+                cmd = "sbatch --job-name={PROCID}.{ANALYSIS}.{MTAG} \
+                -o '{LOGDIR}/{PROCID}.{ANALYSIS}.{MTAG}.out' \
+                -e '{LOGDIR}/{PROCID}.{ANALYSIS}.{MTAG}.err' \
+        /n/coxfs01/2p-pipeline/repos/2p-pipeline/pipeline/python/slurm/decode_by_ncells.sbatch \
+        {EXP} {TRACEID} {RTEST} {OVERLAP} {ANALYSIS} {CVAL} {VAREA} {NCELLS} {DKEY}".format(
+                    PROCID=piper, MTAG=mtag, LOGDIR=logdir,
+                    EXP=EXPERIMENT, TRACEID=traceid, ANALYSIS=analysis_type,
+                    RTEST=responsive_test, OVERLAP=overlap_thr, 
+                    CVAL=c_value, VAREA=visual_area, NCELLS=ncells, DKEY=datakey) 
+                #
             status, joboutput = commands.getstatusoutput(cmd)
             jobnum = joboutput.split(' ')[-1]
             jobids.append(jobnum)
             info("[%s]: %s" % (jobnum, mtag))
-else:
-    # -----------------------------------------------------------------
-    # SINGLE CELLS
-    # -----------------------------------------------------------------
+elif analysis_type in ['by_fov', 'split_pupil']:
     ncells=None
     dsets = load_metadata(EXPERIMENT, visual_area=visual_area)
-    #incl = ['20190614_JC091_fov1', '20190602_JC091_fov1', '20190609_JC099_fov1']
-    #dsets = dsets[dsets['datakey'].isin(incl)]
+    included_datakeys = args.included_datakeys
+    print("dkeys:", included_datakeys)
+    #['20190614_jc091_fov1', '20190602_jc091_fov1', '20190609_jc099_fov1']
+    if included_datakeys is not None: #len(included_datakeys) > 0:
+        dsets = dsets[dsets['datakey'].isin(included_datakeys[0])]
     if len(dsets)==0:
-        fatal("NO FOVs found.")
-    info("Found %i [%s] datasets to process." % (len(dsets), EXPERIMENT))
+        fatal("no fovs found.")
+    info("found %i [%s] datasets to process." % (len(dsets), EXPERIMENT))
+
     for (visual_area, datakey), g in dsets.groupby(['visual_area', 'datakey']):
         mtag = '%s_%s_%s' % (datakey, visual_area, C_str) 
         #
-        cmd = "sbatch --job-name={PROCID}.{MTAG}.{ANALYSIS} \
-                -o '{LOGDIR}/{PROCID}.{MTAG}.{ANALYSIS}.out' \
-                -e '{LOGDIR}/{PROCID}.{MTAG}.{ANALYSIS}.err' \
-                /n/coxfs01/2p-pipeline/repos/2p-pipeline/pipeline/python/slurm/decode_single_cells.sbatch \
-        {EXP} {TRACEID} {RTEST} {OVERLAP} {ANALYSIS} {CVAL} {VAREA} {NCELLS} {DKEY}".format(
-            PROCID=piper, MTAG=mtag, LOGDIR=logdir,
-            EXP=EXPERIMENT, TRACEID=traceid, ANALYSIS=analysis_type,
-            RTEST=responsive_test, OVERLAP=overlap_thr, 
-            CVAL=c_value, VAREA=visual_area, NCELLS=ncells, DKEY=datakey) 
+        cmd = "sbatch --job-name={procid}.{mtag}.{analysis} \
+                -o '{logdir}/{procid}.{mtag}.{analysis}.out' \
+                -e '{logdir}/{procid}.{mtag}.{analysis}.err' \
+        /n/coxfs01/2p-pipeline/repos/2p-pipeline/pipeline/python/slurm/decode_by_ncells.sbatch \
+        {exp} {traceid} {rtest} {overlap} {analysis} {cval} {varea} {ncells} {dkey}".format(
+            procid=piper, mtag=mtag, logdir=logdir,
+            exp=EXPERIMENT, traceid=traceid, analysis=analysis_type,
+            rtest=responsive_test, overlap=overlap_thr, 
+            cval=c_value, varea=visual_area, ncells=ncells, dkey=datakey) 
+        #
+        status, joboutput = commands.getstatusoutput(cmd)
+        jobnum = joboutput.split(' ')[-1]
+        jobids.append(jobnum)
+        info("[%s]: %s" % (jobnum, mtag))
+
+
+else:
+    # -----------------------------------------------------------------
+    # SINGLE CELLS 
+    # -----------------------------------------------------------------
+    ncells=None
+    dsets = load_metadata(experiment, visual_area=visual_area)
+    included_datakeys = args.included_datakeys[0]
+    print("dkeys:", included_datakeys)
+    #['20190614_jc091_fov1', '20190602_jc091_fov1', '20190609_jc099_fov1']
+    if len(included_datakeys) > 0:
+        print(included_datakeys)
+        dsets = dsets[dsets['datakey'].isin(included_datakeys)]
+    if len(dsets)==0:
+        fatal("no fovs found.")
+    info("found %i [%s] datasets to process." % (len(dsets), experiment))
+    for (visual_area, datakey), g in dsets.groupby(['visual_area', 'datakey']):
+        mtag = '%s_%s_%s' % (datakey, visual_area, c_str) 
+        #
+        cmd = "sbatch --job-name={procid}.{mtag}.{analysis} \
+                -o '{logdir}/{procid}.{mtag}.{analysis}.out' \
+                -e '{logdir}/{procid}.{mtag}.{analysis}.err' \
+        /n/coxfs01/2p-pipeline/repos/2p-pipeline/pipeline/python/slurm/decode_single_cells.sbatch \
+        {exp} {traceid} {rtest} {overlap} {analysis} {cval} {varea} {ncells} {dkey}".format(
+            procid=piper, mtag=mtag, logdir=logdir,
+            exp=experiment, traceid=traceid, analysis=analysis_type,
+            rtest=responsive_test, overlap=overlap_thr, 
+            cval=c_value, varea=visual_area, ncells=ncells, dkey=datakey) 
         #
         status, joboutput = commands.getstatusoutput(cmd)
         jobnum = joboutput.split(' ')[-1]
